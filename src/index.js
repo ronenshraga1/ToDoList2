4aa32dbbf78ff8970eec6f07c3b1777b5334cc93
unknown <ronenshraga01@gmail.com>
2021-03-03 09:32:57 +0200

    css changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-
+import './primitiveui.css';
 import './api/server'
 
 ReactDOM.render(
